ad9592756501f86f74560f2e3fffd04fefb21f47
Steven Ferrer <steven.r.ferrer@gmail.com>
2020-07-23 19:44:31 +0800

    added switch and cart example

---
+++ examples/cart/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/stevenferrer/esmaq/gen"
+)
+
+func main() {
+	schema := gen.Schema{
+		Name: "Cart",
+		Pkg:  "main",
+		States: []gen.State{
+			{
+				From: "new",
+				Transitions: []gen.Transition{
+					{
+						To:    "finalizing",
+						Event: "checkout",
+						Callback: gen.Callback{
+							Ins: []gen.Param{{ID: "cartID", V: int64(0)}},
+						},
+					},
+				},
+			},
+			{
+				From: "finalizing",
+				Transitions: []gen.Transition{
+					{
+						To:    "submitted",
+						Event: "submit",
+						Callback: gen.Callback{
+							Ins:  []gen.Param{{ID: "cartID", V: int64(0)}},
+							Outs: []gen.Param{{ID: "orderID", V: int64(0)}},
+						},
+					},
+					{
+						To:    "new",
+						Event: "modify",
+						Callback: gen.Callback{
+							Ins: []gen.Param{{ID: "cartID", V: int64(0)}},
+						},
+					},
+					{
+						To:    "cancelled",
+						Event: "cancel",
+						Callback: gen.Callback{
+							Ins: []gen.Param{{ID: "cartID", V: int64(0)}},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	f, err := os.Create("cart.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = gen.Generate(schema, f)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ examples/switch/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/stevenferrer/esmaq/gen"
+)
+
+func main() {
+	schema := gen.Schema{
+		Name: "Switch",
+		Pkg:  "main",
+		States: []gen.State{
+			{
+				From: "on",
+				Transitions: []gen.Transition{
+					{
+						To:    "off",
+						Event: "switchOff",
+					},
+				},
+			},
+			{
+				From: "off",
+				Transitions: []gen.Transition{
+					{
+						To:    "on",
+						Event: "switchOn",
+					},
+				},
+			},
+		},
+	}
+
+	f, err := os.Create("switch.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = gen.Generate(schema, f)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
